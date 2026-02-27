7836eb1766b0530731886881e427330c1dfdb302
DOMI <YoumiHa@dingtalk.com>
2026-02-27 19:12:16 +0800

    Update McbeUpdatePlayerGameType.cs

--- Protocol/Network/MinecraftPacket/McbeUpdatePlayerGameType.cs
+++ Protocol/Network/MinecraftPacket/McbeUpdatePlayerGameType.cs
@@ -1,7 +1,11 @@
 namespace Protocol.Network.MinecraftPacket;
 public class McbeUpdatePlayerGameType : Packet
 {
-    public McbeUpdatePlayerGameType()
+	public int GameType { get; set; }
+	public long PlayerUniqueID { get; set; }
+	public ulong Tick { get; set; }
+	
+	public McbeUpdatePlayerGameType()
     {
         Id = 0x97;
         IsMcbe = true;
@@ -10,10 +14,16 @@ public class McbeUpdatePlayerGameType : Packet
     protected override void EncodePacket()
     {
         base.EncodePacket();
-    }
+		WriteVarInt(GameType);
+		WriteVarLong(PlayerUniqueID);
+		WriteUnsignedVarLong((long)Tick);
+	}
 
-    protected override void DecodePacket()
+	protected override void DecodePacket()
     {
         base.DecodePacket();
-    }
+		GameType = ReadVarInt();
+		PlayerUniqueID = ReadVarLong();
+		Tick = (ulong)ReadUnsignedVarLong();
+	}
 }
